38657d8084fb0aa81fc22a1ae4511deb19b78ef9
Tomas Dahlander <thomaz.dahlander@hotmail.com>
2021-05-30 15:06:40 +0200

    Changed settings for master branch to be able to run tests correctly

--- src/test/java/com/example/partygamesbackend/controllers/QuestionControllerTest.java
+++ src/test/java/com/example/partygamesbackend/controllers/QuestionControllerTest.java
@@ -1,9 +1,12 @@
 package com.example.partygamesbackend.controllers;
 
 import com.example.partygamesbackend.models.Question;
+import com.example.partygamesbackend.repositories.QuestionRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -23,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureDataJpa
 class QuestionControllerTest {
 
     @Autowired
